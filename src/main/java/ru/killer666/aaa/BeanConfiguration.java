90c8d99083261a8b4baf1288fa9e1ba8b7497318
Kupchinsky <user@killer666.ru>
2015-12-23 09:43:07 +0700

    - Added HibernateSessionService implementation - Removed XML configuration

--- src/main/java/ru/killer666/aaa/BeanConfiguration.java
+++ src/main/java/ru/killer666/aaa/BeanConfiguration.java
@@ -4,15 +4,42 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import ru.killer666.aaa.service.HibernateSessionServiceImpl;
 import ru.killer666.aaa.service.RoleResolverServiceImpl;
+import ru.killer666.trpo.aaa.services.HibernateSessionService;
 import ru.killer666.trpo.aaa.services.RoleResolverService;
 
 @Configuration
 @ComponentScan
 @Import(ru.killer666.trpo.aaa.BeanConfiguration.class)
-public class BeanConfiguration {
+@EnableWebMvc
+public class BeanConfiguration extends WebMvcConfigurerAdapter {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/jsp/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
     @Bean
     RoleResolverService roleResolverService() {
         return new RoleResolverServiceImpl();
+    }
+
+    @Bean
+    public HibernateSessionService hibernateSessionService() {
+        HibernateSessionServiceImpl hibernateSessionService = new HibernateSessionServiceImpl();
+        return hibernateSessionService;
     }
 }
---
+++ src/main/java/ru/killer666/aaa/service/HibernateSessionServiceImpl.java
@@ -0,0 +1,7 @@
+package ru.killer666.aaa.service;
+
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import ru.killer666.trpo.aaa.services.HibernateSessionService;
+
+public class HibernateSessionServiceImpl extends LocalSessionFactoryBean implements HibernateSessionService {
+}
